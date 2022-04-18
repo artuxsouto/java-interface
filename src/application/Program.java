8a50763e86d12eea3b0ca3f20e6c4ec8194f3b4b
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-04-18 19:06:34 -0300

    Solução com interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,10 +42,7 @@ public class Program {
 		System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
 		System.out.println("Tax: " + String.format("%.2f", cr.getInvoice().getTax()));
 		System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().totalPayment()));
-		
-				
+			
 	sc.close();
-
 	}
-
 }
