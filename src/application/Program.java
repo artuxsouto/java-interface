7f57979197332babe2a27f342c51f9731e429a5f
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-04-20 10:22:14 -0300

    Interface Comparable - Aplicando ordenação com implementação na classe e método

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,20 +6,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
+
+import entities.Employee;
 
 public class Program {
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<>();
-		String path = "D:\\out\\Names.txt";
+		
+		Locale.setDefault(Locale.US);
+		List<Employee> list = new ArrayList<>();
+		String path = "D:\\out\\Employees.csv";
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String name = br.readLine();
-			while (name != null) {
-				list.add(name);
-				name = br.readLine();
+			String employeeCsv = br.readLine();
+			while (employeeCsv != null) {
+				String[] fields = employeeCsv.split(",");
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				employeeCsv = br.readLine();
 			}
 			Collections.sort(list);
-			for (String s : list) {
-				System.out.println(s);
+			for (Employee emp : list) {
+				System.out.println(emp.getName() + ", " + String.format("%.2f", emp.getSalary()));
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
